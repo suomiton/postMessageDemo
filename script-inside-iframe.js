e2ea149c3e1c2792dfc69dd323fcb99ff180a55a
Toni Suominen <suomiton@gmail.com>
2014-07-30 23:15:58 +0300

    Added iFrame polling

--- script-inside-iframe.js
+++ script-inside-iframe.js
@@ -1,7 +1,12 @@
 function handleMessage(event) {
 	// domain esimerkiksi http://www.otto.fi
 	if (event.origin === 'Tähän viestin lähettäjän domainin osoite' && event.data === 'window.size.please') {
-	 event.source.postMessage('window size: ' + document.body.scrollHeight + 'x' + document.body.scrollWidth, event.origin);
+		var windowSpecs = {
+            width: document.body.scrollWidth,
+            height: document.body.scrollHeight
+         };
+
+         event.source.postMessage(windowSpecs, event.origin);
 	}
 }
 
--- script-parent-page.js
+++ script-parent-page.js
@@ -1,18 +1,37 @@
-function receiveSizeData(event)
-{  
-	// iframen domain esimerkiksi http://www.kaljaa.com
-	if (event.origin === 'Tähän iframen domainin osoite!') {
-		// event.data pitää sisällään viestin sisällön. 
-		// Tässä kohtaa event.datasta voidaan parsia sivun koko ja muuttaa iframen koko oikeaksi
-		console.log(event.data);
-	}
-}
+// Laitetaan toiminto omaan scopeen ettei se ole suoraan window objektilla. Hyvän tavan mukaisesti.
+// Alustetaan scopeen myös iFrame jotta sitä ei tarvitse hakea domista kuin kerran
+(function(iFrame) {  
+  'use strict';
 
-window.addEventListener("message", receiveSizeData, false);
+  var intervalKey = null,  	// Tätä tarvitaan jotta intervallin saa pysäytettyä halutessaan
+      interval = 200;		// Kuinka usein intervalli toistuu (ms)
 
-window.onload = function() {
+  function receiveSizeData(event)
+  {  
+    if (event.origin === "iFrame-domain-address") {
+    	// debuggausta varten
+		console.log('Window data received. New height is: ' + event.data.height);
 
-	iframe = document.getElementById('content');
-	iframe.contentWindow.postMessage('window.size.please', 'Tähän iframen domainin osoite!');
+		// Viesti sisältää seuraavan objectin
+		// var windowSpecs = {
+  		//     width: document.body.scrollWidth,
+  		//     height: document.body.scrollHeight
+  		// };
 
-};
+  		// Asetetaan iFramelle uusi korkeus
+		iFrame.height = (event.data.height) + 'px';
+    }
+  }
+
+  window.addEventListener("message", receiveSizeData, false);
+
+  window.onload = function() {
+    
+    // Kun sivu on latautunut, käynnistetään iframen pollaus
+    intervalKey = setInterval(function() {
+      iFrame.contentWindow.postMessage('window.size.please', 'iFrame-domain-address');
+    }, interval);    
+
+  };
+  
+})(document.getElementById('content'));
